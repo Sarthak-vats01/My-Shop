dc9692c4f609315b3ee0e656c0fbdce6c1e644ea
Sarthak Vats <sarthakvats121212@gmail.com>
2024-04-18 21:33:42 +0530

    2

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,5 @@
 const USER_URL = "https://my-shop-server.onrender.com/user";
 const PRODUCT_URL = "https://my-shop-server.onrender.com/product";
-const CATEGORY_URL = "https://my-shop-server.onrender.com/category";
+const CATEGORY_URL = "https://my-shop-server.onrender.com/categor";
 
 export { USER_URL, PRODUCT_URL, CATEGORY_URL };
